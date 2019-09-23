e9b170bf1181e146dcbcab7ba75422ff544210fb
brenodiogo <breno.diogo@hotmail.com>
2019-09-23 17:15:14 -0300

    adds places API

--- src/app/pages/mapa/mapa.page.ts
+++ src/app/pages/mapa/mapa.page.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from "@angular/core";
 import { Map, latLng, tileLayer, Layer, marker, leaflet } from "leaflet";
 import { Geolocation } from "@ionic-native/geolocation/ngx";
 
+declare var google;
+
 @Component({
   selector: "app-mapa",
   templateUrl: "./mapa.page.html",
@@ -10,8 +12,9 @@ import { Geolocation } from "@ionic-native/geolocation/ngx";
 export class MapaPage implements OnInit {
   map: Map;
   public isSearchBarOpened = false;
+  map2: any;
 
-  constructor(private geolocation: Geolocation) { }
+  constructor(private geolocation: Geolocation) {}
 
   ngOnInit() {
     // this.leafletMap();
@@ -31,6 +34,7 @@ export class MapaPage implements OnInit {
 
   leafletMap() {
     // In setView add latLng and zoom
+    this.map2 = new google.maps.Map(document.getElementById("map"));
     this.map = new Map("map").setView([-15.8244864, -47.9510528], 10);
     tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
       attribution: "© projeto unisse"
@@ -40,6 +44,8 @@ export class MapaPage implements OnInit {
       .addTo(this.map)
       .bindPopup("Popup!.")
       .openPopup();
+
+    // this.map2 = new google.maps.Map(document.getElementById("map2"));
   }
 
   /** Remove map when we have multiple map object */
@@ -49,6 +55,27 @@ export class MapaPage implements OnInit {
 
   onSearch(event) {
     console.log(event);
+    console.log(event.target.value);
+
+    if (event.target.value.length > 2) {
+      console.log("do google search");
+      var service = new google.maps.places.PlacesService(this.map2);
+      console.log(service);
+      service.textSearch(
+        {
+          location: new google.maps.LatLng(-33.8665433, 151.1956316),
+          radius: "500",
+          query: "restaurant"
+        },
+        this.mapsCallback
+      );
+    }
+  }
+
+  mapsCallback(results, status) {
+    if (status == google.maps.places.PlacesServiceStatus.OK) {
+      console.log (results);
+    }
   }
 
   clickOnSearchIcon(searchBarInput) {
---
+++ src/app/services/map.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MapService {
+
+  private placesApiKey = "AIzaSyBBOOsSxTg_79VMDGqtv_NM9YmIaSaS5oQ";
+
+  constructor() { }
+}
